274ccc72f1eca2ff890b68764c622ef9402e403f
landa44 <pablo44landa@gmail.com>
2022-05-08 11:23:58 -0400

    refactor list view to handle new item POSTs

--- lists/urls.py
+++ lists/urls.py
@@ -21,6 +21,5 @@ urlpatterns = [
     path('', views.home_page, name='home'),
     re_path(r'^new$', views.new_list, name='new_list'),
     re_path(r'^(\d+)/$', views.view_list, name='view_list'),
-    re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 
 ]

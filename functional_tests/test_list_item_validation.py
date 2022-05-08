eec7c692533a0333724c732733e2d801fb2e3011
landa44 <pablo44landa@gmail.com>
2022-05-08 10:08:32 -0400

    Adjust new list view to do model validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -17,10 +17,21 @@ class ItemValidationTest(FunctionalTest):
     ))
     
     #He tries again with some text and it works
-    self.fail('finish this test')
+    self.browser.find_element_by_id('id_new_item').send_keys('Buy milk')
+    self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('1: Buy milk')
 
     #Perversaly, he now decides to submit a second blank list item
-
+    self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+    
     #He receives a similar warning on the list page
+    self.wait_for(lambda: self.assertEqual(
+      self.browser.find_element_by_css_selector('.has-error').text,
+      "You can't have an empty list item"
+    ))
 
     #And he can correct it by filling some text in
+    self.browser.find_element_by_id('id_new_item').send_keys('Buy tea')
+    self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
+    self.wait_for_row_in_list_table('1: Buy milk')
+    self.wait_for_row_in_list_table('2: Buy tea')
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,11 @@
 from crypt import methods
+from xml.dom import ValidationErr
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Item, List
-
+from django.core.exceptions import ValidationError
 
 # Create your views here.
 def home_page(request):
@@ -17,7 +18,15 @@ def view_list(request, list_id):
 def new_list(request):
   list_ = List.objects.create()
 
-  Item.objects.create(text=request.POST['item_text'], list=list_)
+  item = Item(text=request.POST['item_text'], list=list_)
+  try:
+    item.full_clean()
+    item.save()
+  except ValidationError:
+    list_.delete()
+    error = "You can't have an empty list item"
+    return render(request, 'home.html', {"error": error})
+
   return redirect(f"/lists/{list_.id}/")
 
 def add_item(request, list_id):
